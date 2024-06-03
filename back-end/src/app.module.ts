bb93e7871d59335c0726f4e6a3ca5d2f019b267c
Farid <farid.ousmane01@gmail.com~>
2024-06-03 14:51:45 +0200

    changement de la base de projet

--- back-end/src/app.module.ts
+++ back-end/src/app.module.ts
@@ -1,10 +1,22 @@
+// src/app.module.ts
 import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { join } from 'path';
+import { HealthModule } from './health/health.module';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConversationResolver } from './resolvers/conversation.resolver';
+import { MessageResolver } from './resolvers/message.resolver';
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+    }),
+    HealthModule,
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [ConversationResolver, MessageResolver],
 })
-export class AppModule {}
+export class AppModule { }
